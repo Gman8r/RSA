50b53e6772b862b1246d5657c00bf4977b53d4d1
Brian Intile <brianintile@gmail.com>
2018-12-02 19:26:34 -0500

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,47 @@
+
+/**
+ * TODO Class Documentation
+ */
+public class Person
+{
+	private final int BLOCK_SIZE = 3; //ARBITRARY, SUBJECT TO CHANGE (max is hypothetically 8)
+	
+	private long publicKey;		//m = p * q
+	private long privateKey;	//n = (p-1) * (q - 1)
+	private int p;
+	private int q;
+	
+	public long getPublicKey()
+	{
+		return publicKey;
+	}
+	
+	/**
+	 * TODO Create person and generate public/private keys from random primes p and q 
+	 */
+	public Person()
+	{
+		
+	}
+	
+	/**
+	 * TODO encrypt a message using the recipient's public key (use BLOCK_SIZE)
+	 * @param msg
+	 * @param recipient
+	 * @return
+	 */
+	public long[] encryptTo(String msg, Person recipient)
+	{
+		return null;
+	}
+	
+	/**
+	 * TODO decrypt a message with this person's private key (use BLOCK_SIZE)
+	 * @param message
+	 * @return
+	 */
+	public String decrypt(long[] message)
+	{
+		return null;
+	}
+}
---
+++ src/RSA.java
@@ -0,0 +1,127 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;	// Random number generator
+
+/**
+ * TODO Class Documentation
+ */
+public class RSA
+{  
+	
+	//Bergmann's driver
+	public static void main (String args[])
+	{ 	
+		Person Alice = new Person();
+		Person Bob = new Person();
+
+		String msg = new String ("Bob, let's have lunch."); 	// message to be sent to Bob
+		long []  cipher;
+		cipher =  Alice.encryptTo(msg, Bob);			// encrypted, with Bob's public key
+
+		System.out.println ("Message is: " + msg);
+		System.out.println ("Alice sends:");
+		show (cipher);
+
+		System.out.println ("Bob decodes and reads: " + Bob.decrypt (cipher));	// decrypted,
+									// with Bob's private key.
+		System.out.println ();
+
+		msg = new String ("No thanks, I'm busy");
+		cipher = Bob.encryptTo (msg, Alice);
+		
+		System.out.println ("Message is: " + msg);
+		System.out.println ("Bob sends:");
+		show (cipher);
+
+		System.out.println ("Alice decodes and reads: " + Alice.decrypt (cipher));
+	}
+	
+	
+	
+	/**
+	 * TODO (https://dotnetfiddle.net/3nmJrF) use euclidian algorithm to find inv(x) mod m
+	 * @param x
+	 * @param m
+	 * @return
+	 */
+	public static long modInverse(long x, long m) 
+	{
+		return 0;
+	}
+	
+	/**
+	 * TODO (https://dotnetfiddle.net/3nmJrF) raise b to the p power mod m
+	 * @param b
+	 * @param p
+	 * @param m
+	 * @return
+	 */
+	public static long modPower(long b, long p, long m)
+	{
+		return 0;
+	}
+	
+	/**
+	 * TODO (https://dotnetfiddle.net/3nmJrF) helper function for modPower which computes b^n for every power of 2 up to exp2Max
+	 * @param b
+	 * @param exp2Max
+	 * @param m
+	 * @return
+	 */
+	private static long[] binaryPowers(long b, long exp2Max, long m)
+	{
+		return null;
+	}
+	
+	/**
+	 * TODO helper method for multiplying several numbers together, modding them after each multiple
+	 * @param factors 
+	 * @param m
+	 * @return
+	 */
+	private static long modMult(long[] factors, long m)
+	{
+		return 0;
+	}
+	
+	/**
+	 * TODO helper method to return the proper mod of a number (no negatives
+	 * @param x
+	 * @param m
+	 * @return
+	 */
+	public static long trueMod(long x, long m)
+	{
+		return 0;
+	}
+	
+	/**
+	 * TODO return a random prime number between min and max
+	 * @param minValue
+	 * @param maxValue
+	 * @return
+	 */
+	public static long randomPrime(long minValue, long maxValue)
+	{
+		return 0;
+	}
+	
+	/**
+	 * TODO return a random number relatively prime to n, less than n
+	 * @param n
+	 * @return
+	 */
+	private static long randomRelativePrime(long n)
+	{
+		return 0;
+	}
+	
+	/**
+	 * TODO use standard out to display the data as a sequence of numbers
+	 * @param data
+	 */
+	private static void show(long[] data)
+	{
+		
+	}	
+}
