825ad84ea647e7cb6cc2eb3e78251ba16795ec2b
alnutta5 <alnutta5@gnpmf209.rowanads.rowan.edu>
2018-12-08 22:05:18 -0500

    Bug fixes

--- src/Person.java
+++ src/Person.java
@@ -8,14 +8,14 @@
  * December, 2018
  */
 
-import RSA.*;
 import java.lang.*;
+import java.util.Random;
 
 public class Person
 {
 	private final int BLOCK_SIZE = 2; //ARBITRARY, SUBJECT TO CHANGE (max is hypothetically 8)
-	private final int MAX_PRIME_VAL = 1000000; //needs alot more testing
-	private final int MIN_PRIME_VAL = 10000;
+	private final long MAX_PRIME_VAL = 1500; //needs alot more testing
+	private final long MIN_PRIME_VAL = 35;
 	private long publicKey;
 	private long privateKey;
 	private long publicMod;
@@ -44,22 +44,24 @@ public class Person
 	 */
 	public Person()
 	{
+            Random rand = new Random();
 		//p and q will need to have their max values adjusted, this is temporary to keep it fast in testing
-		long p = RSA.randomPrime(MIN_PRIME_VAL, MAX_PRIME_VAL);
-		long q = RSA.randomPrime(MIN_PRIME_VAL, MAX_PRIME_VAL);
+		long p = RSA.randomPrime(MIN_PRIME_VAL, MAX_PRIME_VAL, rand);
+		long q = RSA.randomPrime(MIN_PRIME_VAL, MAX_PRIME_VAL, rand);
 
 		while(p == q) //just in case
 		{
-			q = RSA.randomPrime(MIN_PRIME_VAL, MAX_PRIME_VAL);
+			q = RSA.randomPrime(MIN_PRIME_VAL, MAX_PRIME_VAL, rand);
 		}
 
 		publicMod = p * q;   //TODO: need to add overflow check when dealing with larger primes
-		publicKey = RSA.randomRelativePrime(publicMod);
+		publicKey = RSA.randomRelativePrime((p - 1) * (q - 1), rand);
 
 		long privateHelper = (p-1) * (q-1);
-
+                
 		privateKey = RSA.modInverse(publicKey, privateHelper);    // d = e-1 (mod (p-1)*(q-1))
-	}
+                System.out.println("priv" + privateKey + ", pub: " + p + " & " + q + " is " + publicMod + " with key " + publicKey);
+        }
 
 	/**
 	 * Encrypt a plain text message to recipient.
@@ -74,16 +76,30 @@ public class Person
 		long recipM = recipient.getM();		//recips mod
 
 		int blockArrLength = msg.length() / BLOCK_SIZE;	//arr size
+                if(msg.length() % BLOCK_SIZE > 0)
+                {
+                    blockArrLength++;
+                }
 
 		long block = 0;		//used for storing the current block
 		long[] blockArr = new long[blockArrLength];		//return arr
 
 		int ndx = 0;		//keeping track of actual msg index
 		int blockNDX = 0;	//keeping track of blockArr index
-
+                 
+                
 		while(ndx < msg.length()) {
-			block = RSA.toLong(msg, ndx, BLOCK_SIZE);		//use RSA class's toLong conversion
-			blockArr[blockNdx++] = block;
+                        if(ndx + BLOCK_SIZE > msg.length())
+                        {
+                            block = RSA.toLong(msg, ndx, 1);
+                        }
+                        else{
+                            block = RSA.toLong(msg, ndx, BLOCK_SIZE);
+                        }
+					//use RSA class's toLong conversion
+                        block = RSA.modPower(block, recipE, recipM);
+                        //System.out.print(block + ", ");
+			blockArr[blockNDX++] = block;
 			ndx += BLOCK_SIZE;		//increment by our block size
 		}
 
@@ -111,13 +127,14 @@ public class Person
 		long currentBlock = 0;
 		long plainBlock = 0;
 		StringBuffer buff = new StringBuffer();
-		for(int i = 0; i < message.length(); i++)
+		for(int i = 0; i < message.length; i++)
 		{
-			if(message[i] != null) {
+			//if(message[i] != null) {
 				currentBlock = message[i];
 				plainBlock = RSA.modPower(currentBlock, privateKey, publicMod);
+                                //System.out.print(plainBlock + ", ");
 				buff.append(RSA.longToNChars(plainBlock));
-			}
+			//}
 		}
 
 		return buff.toString();
